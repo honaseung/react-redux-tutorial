2d2267e2407797fdd6e718e1a1664be32d42b8e0
honaseung <lhs5948@gmail.com>
2022-12-27 18:38:41 +0900

    yarn add redux configurations and create files for using redux

---
+++ src/modules/counter.js
@@ -0,0 +1,31 @@
+const INCRESE = "counter/INCRESE";
+const DECRESE = "counter/DECRESE";
+
+export const increse = () => ({ type: INCRESE });
+export const decrese = () => ({ type: DECRESE });
+
+const initialState = {
+  number: 0,
+};
+
+function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCRESE:
+      return {
+        ...state,
+        number: state.number + 1,
+      };
+    case DECRESE:
+      return {
+        ...state,
+        number: state.number - 1,
+      };
+
+    default:
+      return {
+        ...state,
+      };
+  }
+}
+
+export default counter;
